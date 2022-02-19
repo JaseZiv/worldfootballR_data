3c6ff713a08a0ef5f9355b8eba791a899fe68189
Tony ElHabr <anthonyelhabr@gmail.com>
2022-02-19 09:38:31 -0600

    add fotmob league scraper

--- raw-data/job_controller.R
+++ raw-data/job_controller.R
@@ -18,3 +18,8 @@ source(here::here("raw-data", "all_leages_and_cups", "get_all_comp_seasons.R"))
 
 # Scrape Transfermarkt Data -----------------------------------------------
 source(here::here("raw-data", "transfermarkt_leagues", "get_transfermarkt_metadata.R"))
+
+
+# Scrape Fotmob league data ----------------------------------------------
+# source(here::here("raw-data", "fotmob-leagues", "get_fotmob_leagues.R"))
+
