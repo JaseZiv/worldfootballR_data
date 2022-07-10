edd65102aa826f0c7b8aa3cdc6044c4479467250
Tony ElHabr <anthonyelhabr@gmail.com>
2022-07-10 17:42:55 -0500

    add 2 fotmob GHAs

---
+++ R/fotmob_match_details/update_fotmob_match_details.R
@@ -0,0 +1,100 @@
+library(worldfootballR)
+library(dplyr)
+library(purrr)
+library(tidyr)
+library(janitor)
+
+data_dir <- file.path("data", "fotmob_match_details")
+subdata_dir <- file.path(data_dir, "matches")
+dir.create(data_dir, showWarnings = FALSE)
+dir.create(subdata_dir, showWarnings = FALSE)
+
+matches_by_date <- file.path("data", "fotmob_matches_by_date", "matches_by_date.rds") |> readRDS()
+
+
+scrape_fotmob_match_details <- function(match_id, overwrite = FALSE) {
+  path <- file.path(subdata_dir, sprintf("%s.rds", match_id))
+  if(file.exists(path) & !overwrite) {
+    message(sprintf("Returning pre-saved data for %s.", match_id))
+    return(readRDS(path))
+  }
+  message(sprintf("Scraping matches for %s.", match_id))
+  match_details <- fotmob_get_match_details(match_id)
+  saveRDS(match_details, path)
+  match_details
+}
+possibly_scrape_fotmob_match_details <- possibly(scrape_fotmob_match_details, otherwise = tibble(), quiet = FALSE)
+slowly_scrape_fotmob_match_details <- slowly(possibly_scrape_fotmob_match_details, quiet = TRUE)
+
+league_id_mapping <- c(
+  "47" = "ENG",
+  "53" = "FRA",
+  "54" = "GER",
+  "55" = "ITA",
+  "87" = "ESP"
+)
+
+league_start_dates <- league_id_mapping |> 
+  imap_dfr(
+    ~load_match_results(
+      country = .x, 
+      gender = "M", 
+      season_end_year = 2021, 
+      tier = "1st"
+    ) |> 
+      slice_min(Date, n = 1, with_ties = FALSE) |> 
+      select(date = Date) |> 
+      mutate(league_id = .y, .before = 1)
+  )
+league_start_date_mapping <- setNames(league_start_dates$date, league_start_dates$league_id)
+
+slowly_scrape_fotmob_match_details_for_league <- function(league_id) {
+  first_date <- as.Date(league_start_date_mapping[[league_id]])
+  path <- file.path(data_dir, sprintf("%s_match_details.rds", league_id))
+  path_exists <- file.exists(path)
+  if(isTRUE(path_exists)) {
+    existing_match_details <- readRDS(path)
+    existing_match_ids <- unique(existing_match_details$match_id)
+    matches_by_date <- matches_by_date |> 
+      filter(!(match_id %in% existing_match_ids))
+      
+  }
+
+  league_matches_by_date <- matches_by_date |> 
+    filter(date >= !!first_date) |> 
+    filter(primary_id == !!league_id, !match_status_cancelled)
+  
+  if(nrow(league_matches_by_date) == 0) {
+    message(sprintf("Not updating data for `league_id = %s`.", league_id))
+    return(match_details)
+  }
+  
+  scrape_time_utc <- as.POSIXlt(Sys.time(), tz = "UTC")
+  
+  raw_match_details <- league_matches_by_date$match_id |> 
+    map_dfr(possibly_scrape_fotmob_match_details)
+  
+  if(nrow(raw_match_details) == 0) {
+    message(sprintf("Not updating data for `league_id = %s`. Bad matches: %s", league_id, nrow(league_matches_by_date)))
+    return(match_details)
+  }
+  
+  match_details <- raw_match_details |> 
+    unnest(shots) |> 
+    unnest_wider(on_goal_shot, names_sep = "_") |> 
+    clean_names()
+  
+  if(isTRUE(path_exists)) {
+    match_details <- bind_rows(
+      existing_match_details,
+      match_details
+    )
+  }
+  
+  attr(match_details, "scrape_timestamp") <- scrape_time_utc
+  saveRDS(match_details, path)
+  match_details
+}
+
+names(league_id_mapping) |> 
+  walk(slowly_scrape_fotmob_match_details_for_league)
---
+++ R/fotmob_matches_by_date/update_fotmob_matches_by_date.R
@@ -0,0 +1,62 @@
+library(worldfootballR)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(janitor)
+
+data_dir <- file.path("data", "fotmob_matches_by_date")
+subdata_dir <- file.path(data_dir, "dates")
+dir.create(data_dir, showWarnings = FALSE)
+dir.create(subdata_dir, showWarnings = FALSE)
+
+scrape_time_utc <- as.POSIXlt(Sys.time(), tz = "UTC")
+path <- file.path(data_dir, "matches_by_date.rds")
+path_exists <- file.exists(path)
+yesterday <- Sys.Date() - 1
+
+dates <- seq.Date(as.Date("2017-08-11"), yesterday, by = "day")
+if (isTRUE(path_exists)) {
+  existing_matches_by_date <- readRDS(path)
+  existing_dates <- existing_matches_by_date |> 
+    distinct(date) |> 
+    pull(date)
+  dates <- setdiff(dates, existing_dates)
+}
+
+scrape_fotmob_matches_on_date <- function(date, overwrite = FALSE) {
+  path <- file.path(subdata_dir, sprintf("%s.rds", date))
+  if(file.exists(path) & !overwrite) {
+    message(sprintf("Returning pre-saved data for %s.", date))
+    return(readRDS(path))
+  }
+  message(sprintf("Scraping matches for %s.", date))
+  matches <- fotmob_get_matches_by_date(date)
+  saveRDS(matches, path)
+  matches
+}
+possibly_scrape_fotmob_matches_on_date <- possibly(scrape_fotmob_matches_on_date, otherwise = tibble(), quiet = FALSE)
+slowly_scrape_fotmob_matches_on_date <- slowly(possibly_scrape_fotmob_matches_on_date, quiet = TRUE)
+
+raw_matches_by_date <- setNames(dates, dates) |> 
+  map_dfr(
+    slowly_scrape_fotmob_matches_on_date,
+    .id = "date"
+  )
+
+matches_by_date <- raw_matches_by_date |>   
+  rename(match = matches) |> 
+  unnest(match, names_sep = "_") |> 
+  rename(home = match_home, away = match_away) |> 
+  unnest(c(home, away, match_status), names_sep = "_") |> 
+  unnest(c(match_status_reason)) |> 
+  clean_names()
+
+if(isTRUE(path_exists)) {
+  matches_by_date <- bind_rows(
+    matches_by_date |> filter(!(date %in% dates)),
+    matches_by_date
+  ) |> 
+    distinct()
+}
+attr(matches_by_date, "scrape_timestamp") <- scrape_time_utc
+saveRDS(matches_by_date, path)
