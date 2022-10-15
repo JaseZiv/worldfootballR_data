557256a8b3171d5a9f6656c5ea13d230684abd3e
Tony ElHabr <anthonyelhabr@gmail.com>
2022-10-15 12:32:08 -0500

    don't update if there is no need to

--- R/fotmob_matches_by_date/update_fotmob_matches_by_date.R
+++ R/fotmob_matches_by_date/update_fotmob_matches_by_date.R
@@ -17,46 +17,55 @@ existing_dates <- existing_matches_by_date |>
   distinct(date) |> 
   pull(date) |> 
   ymd()
-dates <- as.Date(setdiff(dates, existing_dates), origin = "1970-01-01")
+new_dates <- as.Date(setdiff(dates, existing_dates), origin = "1970-01-01")
 
-scrape_fotmob_matches_on_date <- function(date, overwrite = FALSE) {
-  Sys.sleep(1)
-  message(sprintf("Scraping matches for %s.", date))
-  fotmob_get_matches_by_date(date)
-}
-possibly_scrape_fotmob_matches_on_date <- possibly(scrape_fotmob_matches_on_date, otherwise = tibble(), quiet = FALSE)
-
-matches_by_date <- setNames(dates, dates) |> 
-  map_dfr(
-    possibly_scrape_fotmob_matches_on_date,
-    .id = "date"
-  )
-
-matches_by_date <- bind_rows(
-  existing_matches_by_date |> filter(!(date %in% dates)),
-  matches_by_date
-) |> 
-  distinct()
-
-popular_league_ids <- c(50, 42, 73, 47, 54, 87, 53, 130, 55)
-popular_matches_by_date <- matches_by_date |> 
-  filter(primary_id %in% popular_league_ids)
-
-split(popular_matches_by_date, popular_matches_by_date$primary_id) |> 
-  iwalk(
+if (length(new_dates) == 0) {
+  message("Skipping since there are no new dates.")
+} else {
+  
+  scrape_fotmob_matches_on_date <- function(date, overwrite = FALSE) {
+    Sys.sleep(1)
+    message(sprintf("Scraping matches for %s.", date))
+    fotmob_get_matches_by_date(date)
+  }
+  possibly_scrape_fotmob_matches_on_date <- possibly(scrape_fotmob_matches_on_date, otherwise = tibble(), quiet = FALSE)
+  
+  new_matches_by_date <- new_dates |> 
+    set_names() |> 
+    map_dfr(
+      possibly_scrape_fotmob_matches_on_date,
+      .id = "date"
+    )
+  
+  matches_by_date <- bind_rows(
+    existing_matches_by_date,
+    new_matches_by_date
+  ) |> 
+    distinct()
+  
+  popular_league_ids <- c(50, 42, 73, 47, 54, 87, 53, 130, 55)
+  
+  walk(
+    popular_league_ids,
     ~{
+      new_league_matches_by_date <- new_matches_by_date |> filter(primary_id == .x)
+      if (nrow(new_league_matches_by_date) == 0) {
+        return(invisible())
+      }
+      league_matches_by_date <- matches_by_date |> filter(primary_id == .x)
       attr(.x, "scrape_timestamp") <- scrape_time_utc
       write_worldfootballr_rds_and_csv(
-        x = .x,
+        x = league_matches_by_date,
         name = sprintf("%s_matches_by_date", .y),
         tag = "fotmob_matches_by_date"
       )
     }
   )
 
-attr(matches_by_date, "scrape_timestamp") <- scrape_time_utc
-write_worldfootballr_rds_and_csv(
-  matches_by_date,
-  name = "matches_by_date",
-  tag = "fotmob_matches_by_date"
-)
+  attr(matches_by_date, "scrape_timestamp") <- scrape_time_utc
+  write_worldfootballr_rds_and_csv(
+    matches_by_date,
+    name = "matches_by_date",
+    tag = "fotmob_matches_by_date"
+  )
+}
