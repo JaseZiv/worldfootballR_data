e0510fdbe3124ff518f5f6cb40391c74bb7a3e14
Tony ElHabr <anthonyelhabr@gmail.com>
2023-08-24 13:09:05 -0500

    all the code deletion

--- raw-data/fotmob-leagues/get_fotmob_leagues.R
+++
@@ -1,9 +0,0 @@
-library(here)
-library(worldfootballR)
-all_leagues <- worldfootballR::fotmob_get_league_ids(cached = FALSE)
-
-write.csv(
-  all_leagues, 
-  here::here("raw-data", "fotmob-leagues", "all_leagues.csv"), 
-  row.names = FALSE
-)
