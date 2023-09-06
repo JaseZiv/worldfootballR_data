428f9e7efb629ba9055cf2a703cf01fce7a666c6
Tony ElHabr <anthonyelhabr@gmail.com>
2023-09-06 08:27:45 -0500

    only run advanced stats for big 5 and MLS

--- R/fb_advanced_match_stats/shared_fb_advanced_match_stats.R
+++ R/fb_advanced_match_stats/shared_fb_advanced_match_stats.R
@@ -6,19 +6,24 @@ params <- bind_rows(
     gender = 'M'
   ),
   'other_1st_M' = list(
-    country = c('POR', 'NED', 'BRA', 'MEX', 'USA'),
+    country = 'USA',
     tier = '1st',
     gender = 'M'
   ),
-  '1st_F' = list(
-    country = c('ENG', 'USA'),
-    tier = '1st',
-    gender = 'F'
-  ),
-  '2nd_M' = list(
-    country = c('ENG'),
-    tier = '2nd',
-    gender = 'M'
-  ),
+  # 'other_1st_M' = list(
+  #   country = c('POR', 'NED', 'BRA', 'MEX', 'USA'),
+  #   tier = '1st',
+  #   gender = 'M'
+  # ),
+  # '1st_F' = list(
+  #   country = c('ENG', 'USA'),
+  #   tier = '1st',
+  #   gender = 'F'
+  # ),
+  # '2nd_M' = list(
+  #   country = c('ENG'),
+  #   tier = '2nd',
+  #   gender = 'M'
+  # ),
   .id = 'group'
 )
